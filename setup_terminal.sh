93d5d93f5de8316086e2af3b771f3915af765560
Arthur <arthurnalves@gmail.com>
2022-09-17 08:44:50 +0200

    Create setup_terminal.sh

---
+++ setup_terminal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+# install fish
+sudo apt-add-repository ppa:fish-shell/release-3 -y
+sudo apt update
+sudo apt instal fish
+chsh -s $(which fish)
+
+# install starship (interactive)
+curl -sS https://starship.rs/install.sh | sh
+starship init fish | source
+echo "starship init fish | source" >> ~/.config/fish/config.fish
+
+# fuzzy search (ctrl+R) (interactive)
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
