df23b51be3a5c78eb10684eece09ee48cba71d68
Sprezz <49735154+arthurnalves@users.noreply.github.com>
2024-02-01 09:33:07 +0100

    add: virtualfish and pipx

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -6,8 +6,11 @@ sudo apt upgrade -y
 # install fish
 sudo apt-add-repository ppa:fish-shell/release-3 -y
 sudo apt update
-sudo apt install fish
+sudo apt install fish -y
 chsh -s $(which fish)
+
+# install pipx
+sudo apt install pipx -y
 
 # install starship (interactive)
 curl -sS https://starship.rs/install.sh | sh
@@ -17,3 +20,12 @@ echo "starship init fish | source" >> ~/.config/fish/config.fish
 # fuzzy search (ctrl+R) (interactive)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# virtualfish (inside fish)
+exec fish
+pipx install virtualfish
+fish_add_path /home/arthur/.local/bin
+vf install
+exec fish
+vf addplugins compat_aliases projects environment
+exec fish
